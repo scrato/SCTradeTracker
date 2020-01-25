be1e08c2461b9bfbabbbabe571e526c64d73bb36
Michael Schroeter <m_schroeter@outlook.com>
2020-01-25 10:51:25 +0100

    Add project files.

---
+++ API/Hotkey.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SCTradeTracker.API
+{
+    /// <summary>
+    /// Extracted from <see cref="https://stackoverflow.com/questions/2450373/set-global-hotkeys-using-c-sharp"/>
+    /// </summary>
+    public sealed class Hotkey : IDisposable
+    {
+        // Registers a hot key with Windows.
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+        // Unregisters the hot key with Windows.
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        /// <summary>
+        /// Represents the window that is used internally to get the messages.
+        /// </summary>
+        private class Window : NativeWindow, IDisposable
+        {
+            private static int WM_HOTKEY = 0x0312;
+
+            public Window()
+            {
+                // create the handle for the window.
+                this.CreateHandle(new CreateParams());
+            }
+
+            /// <summary>
+            /// Overridden to get the notifications.
+            /// </summary>
+            /// <param name="m"></param>
+            protected override void WndProc(ref Message m)
+            {
+                base.WndProc(ref m);
+
+                // check if we got a hot key pressed.
+                if (m.Msg == WM_HOTKEY)
+                {
+                    // get the keys.
+                    Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);
+                    ModifierKeys modifier = (ModifierKeys)((int)m.LParam & 0xFFFF);
+
+                    // invoke the event to notify the parent.
+                    if (KeyPressed != null)
+                        KeyPressed(this, new KeyPressedEventArgs(modifier, key));
+                }
+            }
+
+            public event EventHandler<KeyPressedEventArgs> KeyPressed;
+
+            #region IDisposable Members
+
+            public void Dispose()
+            {
+                this.DestroyHandle();
+            }
+
+            #endregion
+        }
+
+        private Window _window = new Window();
+        private int _currentId;
+
+        public Hotkey()
+        {
+            // register the event of the inner native window.
+            _window.KeyPressed += delegate (object sender, KeyPressedEventArgs args)
+            {
+                if (KeyPressed != null)
+                    KeyPressed(this, args);
+            };
+        }
+
+        /// <summary>
+        /// Registers a hot key in the system.
+        /// </summary>
+        /// <param name="modifier">The modifiers that are associated with the hot key.</param>
+        /// <param name="key">The key itself that is associated with the hot key.</param>
+        public void RegisterHotKey(HotkeyArgs args)
+        {
+            // increment the counter.
+            _currentId = _currentId + 1;
+
+            // register the hot key.
+            if (!RegisterHotKey(_window.Handle, _currentId, (uint)args.Modifier, (uint)args.Key))
+                throw new InvalidOperationException("Couldn’t register the hot key.");
+        }
+
+        /// <summary>
+        /// A hot key has been pressed.
+        /// </summary>
+        public event EventHandler<KeyPressedEventArgs> KeyPressed;
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            // unregister all the registered hot keys.
+            for (int i = _currentId; i > 0; i--)
+            {
+                UnregisterHotKey(_window.Handle, i);
+            }
+
+            // dispose the inner native window.
+            _window.Dispose();
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Event Args for the event that is fired after the hot key has been pressed.
+    /// </summary>
+    public class KeyPressedEventArgs : EventArgs
+    {
+        public HotkeyArgs Args { get; }
+
+        internal KeyPressedEventArgs(HotkeyArgs args)
+        {
+            Args = args;
+        }
+
+        
+
+    }
+
+    /// <summary>
+    ///  The class for combining Modifier and Key as one Combination
+    /// </summary>
+    public class HotkeyArgs
+    {
+        public ModifierKeys Modifier { get; }
+        public Keys Key { get; }
+
+        public HotkeyArgs(ModifierKeys modifier, Keys key)
+        {
+            Modifier = modifier;
+            Key = key;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as HotkeyArgs;
+            if (item == null) return false;
+            return item.Modifier.Equals(Modifier) && item.Key.Equals(Key);
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            hash = hash * 31 + Modifier.GetHashCode();
+            hash = hash * 31 + Key.GetHashCode();
+            return hash;
+        }
+
+        public static bool operator ==(HotkeyArgs obj1, HotkeyArgs obj2) => obj1.Equals(obj2);
+        public static bool operator !=(HotkeyArgs obj1, HotkeyArgs obj2) => !obj1.Equals(obj2);
+    }
+
+    /// <summary>
+    /// The enumeration of possible modifiers.
+    /// </summary>
+    [Flags]
+    public enum ModifierKeys : uint
+    {
+        Alt = 1,
+        Control = 2,
+        Shift = 4,
+        Win = 8
+    }
+}
---
+++ App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using SCTradeTracker.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+using SCTradeTracker.API;
+
+namespace SCTradeTracker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        private Hotkey hotkey;
+
+        const ModifierKeys printModifier = ModifierKeys.Control | ModifierKeys.Alt;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            hotkey = new Hotkey();
+            hotkey.RegisterHotKey(, System.Windows.Forms.Keys.Print);
+            hotkey.KeyPressed += Hotkey_KeyPressed;
+        }
+
+        private void Hotkey_KeyPressed(object sender, KeyPressedEventArgs e)
+        {
+            if(e.)
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
